69c02b2f718b6c97313918969a25f4000215f657
Andreas Copan <avcopan@gmail.com>
2015-10-31 14:07:17 -0400

    added memory allocation

--- 01.cpp
+++ 01.cpp
@@ -1,6 +1,7 @@
 #include <iostream>   // std::cout, std::endl
 #include <functional> // std::multiplies
 #include <numeric>    // std::accumulate
+#include <boost/shared_ptr.hpp> // boost::shared_ptr
 
 template<int k, typename T>
 class Array {
@@ -13,12 +14,13 @@ class Array {
 
     int _nelem;
 
-    T* _data;
+    boost::shared_ptr<T[]> _data;
 
     template<typename... Args>
     Array(Args&&... args) : _shape{args...} {static_assert(sizeof...(args) == k, "Requested shape does not match rank.");
       for(int i=0; i<k; ++i) _stride[i] = std::accumulate(_shape + 1, _shape + (k-i), 1, std::multiplies<int>());
       _nelem = std::accumulate(_shape, _shape + k, 1, std::multiplies<int>());
+      _data  = boost::shared_ptr<T[]>(new T[_nelem]);
     }
 
 };
@@ -28,6 +30,9 @@ int main() {
 
   for(int i=0; i<5; ++i)
     std::cout << A._stride[i] << std::endl;
+
+  for(int i=0; i<120; ++i)
+    std::cout << A._data[i] << std::endl;
 
   std::cout << A._nelem << std::endl;
 }
